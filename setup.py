1350652bb8e2c8c95cab602430246cbab004d250
Laurent Almeras <laurent.almeras@openwide.fr>
2017-02-21 19:38:44 +0100

    Bump version: 0.2.0 → 0.2.1rc

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='powo_owinstall',
-    version='0.2.0',
+    version='0.2.1rc',
     description="Open Wide base installation (powo plugin)",
     long_description=readme + '\n\n' + history,
     author="Laurent Almeras",
